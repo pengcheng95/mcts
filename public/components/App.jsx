1da1b68776d83d0551e9565a2e8b4f5b29ba2d1a
Max Peng <pengcheng95@gmail.com>
2018-01-14 15:52:02 -0500

    refactor file structure

---
+++ public/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+  return() {
+    return (
+      <div> 
+        <h2> Tic Tac Toe </h2>
+
+
+      </div>
+      )
+  }
+}
